bcd6eed4f95ee3fb0653ec32e9e569dae5dfe8f7
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-08-04 22:02:25 -0400

    Extra footer removed

--- PortfolioProject/Scripts/SumOfAllFears.js
+++ PortfolioProject/Scripts/SumOfAllFears.js
@@ -29,34 +29,50 @@
 
         var values = [num1, num2, num3, num4, num5, num6];
         var sums = [];
+        var sol1 = [];
+        var sol2 = [];
 
         for (var i = 0; i < values.length - 1; i++) {
-
 
             for (var j = 0; j < values.length - 1; j++) {
 
                 if ((values[i] + values[j + 1])) {
-                    sums.push(values[i] + values[j + 1]); //Prevents the same number from being added to itself
+                    sol1.push(values[i]);
+                    sol2.push(values[j + 1]);
+                    sums.push(values[i] + values[j + 1]);
                 }
 
             }
         }
 
-        for (var num = 0; num < sums.length; num++) {
+        for (var num = 0; num < sums.length-1; num++) {
 
             if (k == sums[num]) {
-                $("#kOut").text("The k-value " + sums[num] + " has been found.");
-                return; //This needs to be here to stop the else statement from executing. Some values will not equal k causing both
-                //statements to want to execute.
+                $("#kOut").text("The k-value " + sums[num] + " has been found by adding " + sol1[num] + " and " + sol2[num]);
+                return;
 
-                //YOU SHOULD STILL BE ABLE TO PUT SOMETHING HERE TO CHECK FOR THE I AND J VALUES RESPONSIBLE. It isn't like any of this
-                //changes the user input.
             }
 
             else {
                 $("#kOut").text("The k-value has not been found");
             }
         }
+
+        /*
+            if (k == (sum[0] + sum[1])) {
+                $("#kOut").text("The k-value " + k + " has been found by adding " + sum[0] + " and " + sum[1]);
+                //return; //This needs to be here to stop the else statement from executing. Some values will not equal k causing both
+                //statements to want to execute.
+
+                //YOU SHOULD STILL BE ABLE TO PUT SOMETHING HERE TO CHECK FOR THE I AND J VALUES RESPONSIBLE. It isn't like any of this
+                //changes the user input.
+            }
+
+            else
+            {
+                $("#kOut").text("The k-value has not been found");
+            }
+            */
     });
 
     $("#clrBtn5").click(function () {
